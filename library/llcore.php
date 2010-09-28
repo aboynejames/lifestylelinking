620522fcbb7071579fddd67624c5e3e413fb28c3
Andrew Sage <andrew@asae.co.uk>
2010-09-28 13:40:35 +0100

    llcore class outlined

--- library/llcore.php
+++ library/llcore.php
@@ -3,34 +3,54 @@
 
 class llcore {
 
+	// populate the index array from a string of text
+	public function populateArray($input)
+	{
+		
+	}
 
-// input text array
+	// build the definitions from the wikipedia description
+	// defintions and those to be scored
+	public function buildDefinitions()
+	{
+		// Call the Wikipedia API
+	}
+	
+	// tidy data, excluded words (need to crowd source these via confusion )
+	public function cleanseData()
+	{
+		
+	}
+	
+	public function createLLMatrix()
+	{
+		// score matrix
+		// sub processes,    word frequency, def and posts(input) match top20 and top50 (create code to test/experiment no. of words and matching logic)
+	}
 
-// defintions and those to be scored
+	// cal stats
+	public function calculateLLStats()
+	{
+		
+	}
+	
+	// average of averages
+	public function calculateLLAvgOfAvg()
+	{
+		
+	}
 
+	// calc melife
+	public function calculateLLNormalisation()
+	{
+		
+	}
 
-// tidy data, excluded words (need to crowd source these via confusion )
-
-// score matrix
-    // sub processes,    word frequency, def and posts(input) match top20 and top50 (create code to test/experiment no. of words and matching logic)
-
-// cal stats
-
-// calc melife
-
-// average of averages
-
-// peergroups (maybe a different class for variations)
-
-
-
-
-
-
-
-
+	// peergroups (maybe a different class for variations)
+	public function buildLLGroups()
+	{
+		
+	}
 }  // closes llcore class
-
-
 
 ?>
