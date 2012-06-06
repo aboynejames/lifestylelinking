e55f300cbf459923e1d47f6da0e26de351afed98
aboynejames <james@aboynejames.co.uk>
2012-06-06 11:19:32 +0100

    restructure folder structure of CI jenkins

--- display/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-//print_r($_SERVER);
-site::includeTemplate('header.php');
-
-site::includeTemplate('body.php');  // basic page html for ajax injection
-
-site::includeTemplate('footer.php');
-
-?>
--- library/logic/llsocial.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// ll social  perparation of personalized peer group (context defintions, time, media) and 'average peer groups'
-
-
-
-
-
-?>
