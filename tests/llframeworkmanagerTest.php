6fa712ba6f5d3dfe0c5d26f86d533bbd9bd6acbe
aboynejames <james@aboynejames.co.uk>
2012-06-06 11:03:18 +0100

    first unit test added

---
+++ tests/llframeworkmanagerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once("lifestylelinking/llframeworkmanager.php");
+
+class llframeworkmanagerTest extends PHPUnit_Framework_TestCase {
+
+    public function startLLframework() {
+        $x = new LLframeworkmanager();
+//        $this->assertEquals(1, $x->demo(1));
+    }
+}
+
+?>
