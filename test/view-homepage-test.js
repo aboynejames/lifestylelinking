07f020212ca9e2c652b1af4109712e0f52ec18d7
aboynejames <james@aboynejames.co.uk>
2012-07-27 14:03:39 +0100

    remove test casper test file

--- test/view-homepage-test.js
+++ test/view-homepage-test.js
@@ -4,15 +4,17 @@
 
 var casper = require("casper").create();
 
-var baseUrl = "http://localhost:8866";
+var baseUrl = "http://localhost:8866/";
 
 casper.test.comment("Scenario: A user can view the home page");
 
 casper.start(baseUrl, function() {
-  this.test.assertHttpStatus(200, "Response is a success");
+  this.test.assertHttpStatus(200, "Response is a success if 200 else a fail");
   this.test.assertTitle("LifestyleLinking", "Title is as expected");
 });
 
+
 casper.run(function() {
+  this.test.renderResults(true, 0, 'log.xml');
   this.test.done();
 });
