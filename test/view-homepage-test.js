224cfae45f29ae7ff062305f1d98fa2681f1f3dc
aboynejames <james@aboynejames.co.uk>
2013-01-15 17:50:08 +0000

    view homepage title test changed

--- test/view-homepage-test.js
+++ test/view-homepage-test.js
@@ -8,7 +8,7 @@ casper.test.comment("Scenario: A user can view the home page");
 casper.start(baseUrl, function() {
 	this.test.comment('when homepage loaded check its title');
   this.test.assertHttpStatus(200, "Response is a success if 200 else a fail");
-  this.test.assertTitle("LifestyleLinking - open source project", "Title is as expected or else fail");
+  this.test.assertTitle("LifestyleLinkingaa - open source project", "Title is as expected or else fail");
 });
 
 casper.run(function() {
